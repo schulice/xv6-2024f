9fe5e7c15ac5ca7cf6e391768cbb33543766dd65
chenzx <lyxx2413@outlook.com>
2025-01-09 03:13:37 +0800

    lab(syscall): attack

--- user/attack.c
+++ user/attack.c
@@ -9,5 +9,20 @@ main(int argc, char *argv[])
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
 
+  // char* current = sbrk(PGSIZE * 32);
+  // for (int i = 0; i < PGSIZE * 32; i += PGSIZE) {
+  //   *(current + i + 32 + 8) = 0;
+  //   printf("block %d content: %s\n", i / PGSIZE, current + i + 32);
+  // }
+  //
+  //
+  // 32 - 9 - 2 - 2 - 2 = 17
+  // ELF - 2, struct proc, trapframe, barrier, stack
+
+  int blkpos = 17;
+  char *end = sbrk(PGSIZE*blkpos);
+  end = end + (blkpos - 1) * PGSIZE;
+  write(2, end + 32, 8);
+  
   exit(1);
 }
