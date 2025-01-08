5de425a46476a46c05812bddb93d08937a6d894e
chenzx <lyxx2413@outlook.com>
2025-01-08 20:34:10 +0800

    lab(syscall): trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -308,6 +308,9 @@ fork(void)
       np->ofile[i] = filedup(p->ofile[i]);
   np->cwd = idup(p->cwd);
 
+  // trace
+  np->tracemask = p->tracemask;
+
   safestrcpy(np->name, p->name, sizeof(p->name));
 
   pid = np->pid;
